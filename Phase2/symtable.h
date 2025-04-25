aa60be4b0f27178f2cbacbae43726847475e1d5e
antreassin <antreassin16@gmail.com>
2025-04-25 18:07:26 +0300

    mikro update sto symtable

--- Phase2/symtable.h
+++ Phase2/symtable.h
@@ -6,72 +6,54 @@
 
 using namespace std;
 
-
-struct Symbol{
+class Symbol{
     string name;
     int scope;
     int line;
     int type;
     bool isActive;
-
-    Symbol(const string& name = "", int scope = 0, int line = 0, int type = 0)
-        : name(name), scope(scope), line(line), type(type), isActive(true) {}
-};
-
-struct Variable{
-    string name;
-    int scope;
-    int line;
-    int type;
-
-    Variable() : name(""), scope(0), line(0), type(0) {}
-
-    Variable(string name, int scope, int line, int type)
-        : name(name), scope(scope), line(line), type(type) {}
-};
-
-struct Function{
-    string name;
-    int scope;
-    int line;
-    int type;
-
-    Function() : name(""), scope(0), line(0), type(0) {}
-
-    Function(string name, int scope, int line, int type)
-        : name(name), scope(scope), line(line), type(type) {}
-};
-
-enum class SymbolType {
-    GLOBALVAR,
-    LOCALVAR,
-    FORMALARG,
-    USERFUNC,
-    LIBFUNC
-};
-//g++ lexLib.cpp al.cpp comment.cpp parser.cpp main.cpp
-class SymbolTable {
-private:
-    unordered_map<string, Symbol> table;
-    unordered_map<string, Symbol> scopeTable;
-    unordered_map<string, vector<string> > nameTable;
-
-
-    int scope;
-
-public:
-
-    SymbolTable();
-    ~SymbolTable();
-    int getScope() const;
-    Symbol* insertSymbol(string name, int scope, int line, int type);
-    Symbol* lookupInScope(const string& name, int targetScope);
-    Symbol* lookupFunction(const string& name, int targetScope);
-    Symbol* lookupExtra(const string& name, bool isGlobal);  
-    bool lookup(string name);
-    vector<Symbol*> getAllSymbolsWithName(const string& name);
-    void shadow();
-    const unordered_map<string, Symbol>& getTable() const {
-        return table;
+    string value;
+    public:
+    Symbol(string name, int scope, int line, int type, string value) : name(name), scope(scope), line(line), type(type), isActive(true) {
+        this->name = name;
+        this->scope = scope;
+        this->line = line;
+        this->type = type;
+        this->isActive = true;
+        this -> value = value;
     }
+    int getScope() const{
+        return scope;
+    }
+    int getLine() const{
+        return line;
+    }
+    int getType() const{
+        return type;
+    }
+    string getName() const{
+        return name;
+    }
+    bool isActive() const{
+        return isActive;
+    }
+    void setActive(bool active){
+        isActive = active;
+    }
+    string getValue() const{
+        return value;
+    }
+    void setValue(string value){
+        this->value = value;
+    }
+};
+
+class SymbolTable{
+    int size;
+    int scope;
+    unordered_map<string, Symbol> scopeTable;
+    public:
+    bool add(string name, int scope, int line, int type);
+    Symbol * lookupInScope(const string& name, int targetScope);
+    bool remove(string name, int scope);
 };
