915cb11f359c10e021f9d13fdd2241c7966f92cc
antreassin <antreassin16@gmail.com>
2025-05-21 01:27:55 +0300

    while loops && break

--- Phase2/headerLib.h
+++
@@ -1,8 +0,0 @@
-#ifndef __HEADER_LIB_
-#define __HEADER_LIB_
-
-#include "symtable.h"
-#include "expressions.h"
-#include "quad.h"
-
-#endif
--- Phase2/yaccHeader.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef YACC_HEADER_HPP
-#define YACC_HEADER_HPP
-#include <cstdio>
-extern int yylineno;
-extern char* yytext;
-extern FILE* yacc_out;
-int yyerror(char* str)
-{return 0;}
-#endif
